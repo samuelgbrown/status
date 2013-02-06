818529a4aa856b3c72cbd198caaae68774fe9cca
Ben Collins <bencollins544@googlemail.com>
2013-02-06 16:11:26 +0000

    Added BBC News

--- feeds/articles.php
+++ feeds/articles.php
@@ -1,6 +1,6 @@
 <?php
- error_reporting(E_WARNINGS);
- ini_set("display_errors", 1);
+ error_reporting(E_ALL);
+ ini_set("display_errors", 0);
 
 $page = ($_GET['page'])?$_GET['page']:1;
 $limit = 10;
@@ -11,9 +11,9 @@ header('Content-type: application/json');
 function getUrl($url)
 {
 
-    $ch = curl_init(); 
+    $ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, $url); 
+	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_USERAGENT, "@status bot v0.1");
 	return curl_exec($ch);
@@ -36,6 +36,8 @@ function aasort (&$array, $key) {
 }
 
 
+$bbc_articles_raw = simplexml_load_string(getUrl('http://feeds.bbci.co.uk/news/technology/rss.xml'));
+
 $hn_articles = json_decode(getUrl('http://api.ihackernews.com/page?format=json'));
 
 $reddit_programming = json_decode(getUrl('http://www.reddit.com/r/programming.json'));
@@ -46,11 +48,10 @@ $reddit_netsec = json_decode(getUrl('http://www.reddit.com/r/netsec.json'));
 
 $reddit_javascript = json_decode(getUrl('http://www.reddit.com/r/javascript.json'));
 
-
 $articles = null;
 
 foreach ($hn_articles->items as $article) {
-	$articles[md5($article->url)]['title'] = $article->title;	
+	$articles[md5($article->url)]['title'] = $article->title;
 	$articles[md5($article->url)]['url'] = $article->url;
 	$articles[md5($article->url)]['sources']["hacker_news"]['title'] = $article->title;
 	$articles[md5($article->url)]['sources']["hacker_news"]['points'] = $article->points;
@@ -58,12 +59,35 @@ foreach ($hn_articles->items as $article) {
 
 }
 
+foreach ($bbc_articles_raw->channel->item as $bbc_article) {
+	$link = str_replace("#sa-ns_mchannel=rss&ns_source=PublicRSS20-sa", "", $bbc_article->link);
+	if(!$articles[md5($link)])
+	{
+		$articles[md5($link)]['title'] = $bbc_article->title;
+		$articles[md5($link)]['url'] = $link;
+	}
+	$articles[md5($link)]['sources']["bbc_news"]['title'] = $bbc_article->title;
+	$articles[md5($link)]['sources']["bbc_news"]['description'] = $bbc_article->description;
+	$shares = json_decode(getUrl('http://open.bbci.co.uk/buzz/shares?url='.$link));
+	if($shares->facebook && $shares->twitter)
+	{
+		$share_count  = ($shares->facebook + $shares->twitter);
+	}
+	else
+	{
+		$share_count = 0;
+	}
+	$articles[md5($link)]['sources']["bbc_news"]['shares'] = $share_count;
+	$articles[md5($link)]['rank'] = $share_count;
+	$link = $shares = $share_count = null;
+}
+
 foreach ($reddit_programming->data->children as $article) {
 	if(!$articles[md5($article->data->url)]){
-		$articles[md5($article->data->url)]['title'] = $article->data->title;	
+		$articles[md5($article->data->url)]['title'] = $article->data->title;
 		$articles[md5($article->data->url)]['url'] = $article->data->url;
 	}
-	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;	
+	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;
 	$articles[md5($article->data->url)]['sources']["reddit"]['up_votes'] = $article->data->ups;
 	$articles[md5($article->data->url)]['sources']["reddit"]['subreddit'] = "programming";
 	$articles[md5($article->data->url)]['rank'] = $articles[md5($article->data->url)]['sources']['rank'] + $article->data->ups;
@@ -72,10 +96,10 @@ foreach ($reddit_programming->data->children as $article) {
 
 foreach ($reddit_netsec->data->children as $article) {
 	if(!$articles[md5($article->data->url)]){
-		$articles[md5($article->data->url)]['title'] = $article->data->title;	
+		$articles[md5($article->data->url)]['title'] = $article->data->title;
 		$articles[md5($article->data->url)]['url'] = $article->data->url;
 	}
-	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;	
+	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;
 	$articles[md5($article->data->url)]['sources']["reddit"]['up_votes'] = $article->data->ups;
 	$articles[md5($article->data->url)]['sources']["reddit"]['subreddit'] = "netsec";
 	$articles[md5($article->data->url)]['rank'] = $articles[md5($article->data->url)]['sources']['rank'] + $article->data->ups;
@@ -84,10 +108,10 @@ foreach ($reddit_netsec->data->children as $article) {
 
 foreach ($reddit_android->data->children as $article) {
 	if(!$articles[md5($article->data->url)]){
-		$articles[md5($article->data->url)]['title'] = $article->data->title;	
+		$articles[md5($article->data->url)]['title'] = $article->data->title;
 		$articles[md5($article->data->url)]['url'] = $article->data->url;
 	}
-	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;	
+	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;
 	$articles[md5($article->data->url)]['sources']["reddit"]['up_votes'] = $article->data->ups;
 	$articles[md5($article->data->url)]['sources']["reddit"]['subreddit'] = "android";
 	$articles[md5($article->data->url)]['rank'] = $articles[md5($article->data->url)]['sources']['rank'] + $article->data->ups;
@@ -96,31 +120,18 @@ foreach ($reddit_android->data->children as $article) {
 
 foreach ($reddit_javascript->data->children as $article) {
 	if(!$articles[md5($article->data->url)]){
-		$articles[md5($article->data->url)]['title'] = $article->data->title;	
+		$articles[md5($article->data->url)]['title'] = $article->data->title;
 		$articles[md5($article->data->url)]['url'] = $article->data->url;
 	}
-	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;	
+	$articles[md5($article->data->url)]['sources']["reddit"]['title'] = $article->data->title;
 	$articles[md5($article->data->url)]['sources']["reddit"]['up_votes'] = $article->data->ups;
 	$articles[md5($article->data->url)]['sources']["reddit"]['subreddit'] = "javascript";
 	$articles[md5($article->data->url)]['rank'] = $articles[md5($article->data->url)]['sources']['rank'] + $article->data->ups;
 
 }
-/*$articles[$i]
-foreach ($articles as $value) {
-	if ( $i < ($startPos+$limit))
-	{
-		$output[] = $value;
-	}
-	else
-	{
-		break;
-	}
-	$i++;
-}*/
-
 
 aasort($articles,"rank");
 
-$stories['stories']=$articles;//$output;
+$stories['stories']=$articles;
 
 print_r(json_encode($stories));
